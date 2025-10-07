d50b0a005de26041f22c6b77e420e05aed8e6512
Alexandre R Paiva <alexrochapaiva@hotmail.com>
2025-10-07 20:06:08 -0300

    Adição de uma depêndencia que faltou (OpenApi) e da implementação em program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Hortifruti.Data;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddControllers();  
+builder.Services.AddApplicationServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers(); // Precisa ser colocado antes do MapOpenApi e do MapScalarApiReference, pois os dois se baseiam nesse mapeamento dos controllers(?)
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Layout = ScalarLayout.Classic;
+        options.Theme = ScalarTheme.Moon;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.Run();
