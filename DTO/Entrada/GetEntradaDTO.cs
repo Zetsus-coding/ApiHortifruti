b90a0f094539f6325cdb4fe4ac44c46bff094bd7
Zetsus <110355970+Zetsus-coding@users.noreply.github.com>
2025-11-24 14:24:35 -0300

    Correção do dto de entrada ("completa")

--- DTO/Entrada/GetEntradaDTO.cs
+++ DTO/Entrada/GetEntradaDTO.cs
@@ -1,16 +1,11 @@
-using ApiHortifruti.DTO.ItemEntrada;
-
-namespace ApiHortifruti.DTO.Entrada;
-
 public class GetEntradaDTO
 {
-    public int Id { get; set; }
-    public int FornecedorId { get; set; }
-    public string NomeFantasiaFornecedor { get; set; } = null!;
+    // public int Id { get; set; }
+    public string NumeroNota { get; set; }
+    public string NomeFantasiaFornecedor { get; set; }
     public int MotivoMovimentacaoId { get; set; }
-    public string Motivo { get; set; } = null!;
+    public string Motivo { get; set; }
     public decimal PrecoTotal { get; set; }
     public DateOnly DataCompra { get; set; }
-    public string NumeroNota { get; set; } = null!;
-    public ICollection<ItemEntradaDTO> Itens { get; set; } = new List<ItemEntradaDTO>();
+    public IEnumerable<ItemEntradaDTO> Itens { get; set; } = new List<ItemEntradaDTO>();
 }
